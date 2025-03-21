47756d35ee28d0db0735a24fa974651a831d48a6
Jessica McPhaul <jmcphaul@smu.edu>
2025-03-21 16:34:49 -0500

    changed stuff

--- script.js
+++ script.js
@@ -35,6 +35,21 @@ async function sendMessage() {
   document.getElementById("userInput").value = "";
 }
 
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-images img');
 
+function showSlide(index) {
+    images.forEach(img => img.classList.remove('active'));
+    images[index].classList.add('active');
+}
 
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showSlide(currentIndex);
+}
 
